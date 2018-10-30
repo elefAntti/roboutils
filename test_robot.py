a6ceb9e971feb857adecefe178075e93b6dc4e86
Antti Valli <antti.valli@gmail.com>
2018-10-30 23:15:19 +0200

    test robot behavior for simulator

---
+++ test_robot.py
@@ -0,0 +1,22 @@
+from roboutils.behavior.robot import FeelTheWayWithBumpers
+from roboutils.hal.remote import SendCommandAndReadSensors
+from roboutils import hal
+from roboutils.utils import kinematics as kine
+from roboutils.behavior import ParallelAll, run, Task
+from roboutils.behavior.time import RateLimit
+
+kinematics = kine.KinematicModel(axel_width = 0.2, left_wheel_r = 0.03, right_wheel_r = 0.03)
+robot_state = hal.RobotInterface(kinematics)
+robot_state.has_left_bumper = True
+robot_state.has_right_bumper = True
+tree = \
+    RateLimit(
+        ParallelAll(
+            FeelTheWayWithBumpers(robot_state, 0.14),
+            SendCommandAndReadSensors(
+                robot_state,
+                local_port = 8001,
+                remote_address = ('localhost', 8000)))
+        , 0.03)
+
+run(tree)
