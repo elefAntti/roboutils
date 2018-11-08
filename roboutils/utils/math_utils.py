efa3be0e3337c822436e496c86f1837bf4d2abc9
Antti Valli <antti.valli@gmail.com>
2018-11-08 23:58:08 +0200

    Add types

--- roboutils/utils/math_utils.py
+++ roboutils/utils/math_utils.py
@@ -1,13 +1,13 @@
 import math
 
-def sign(flt):
+def sign(flt: float) -> float:
     if flt >= 0:
         return 1
     else:
         return -1
 
-def normalizeAngle(angle):
+def normalizeAngle(angle: float) -> float:
     return angle - math.pi * 2.0 * math.floor((angle + math.pi) / (math.pi * 2.0))
 
-def deg2rad(angle):
+def deg2rad(angle: float) -> float:
     return angle * math.pi / 180.0
--- roboutils/utils/vec2.py
+++ roboutils/utils/vec2.py
@@ -8,82 +8,82 @@ class Vec2(namedtuple('Vec2', ['x', 'y'])):
     _zero = None
 
     @classmethod
-    def zero(cls):
+    def zero(cls) -> 'Vec2':
         if not cls._zero:
             cls._zero = Vec2(0,0)
         return cls._zero
 
     @property
-    def length(self):
+    def length(self) -> float:
         return math.hypot(self.x, self.y)
     
     @property
-    def lengthSq(self):
+    def lengthSq(self) -> float:
         return self.dot(self)
 
     @property
-    def heading(self):
+    def heading(self) -> float:
         return math.atan2(self.y, self.x)
 
     @staticmethod
-    def fromPolar(heading, length):
+    def fromPolar(heading: float, length: float) -> 'Vec2':
         return Vec2(
             x=math.cos(heading) * length,
             y=math.sin(heading) * length)
 
-    def __neg__(self):
+    def __neg__(self) -> 'Vec2':
         return Vec2(-self.x, -self.y)
 
-    def __add__(self, other):
+    def __add__(self, other: 'Vec2') -> 'Vec2': # type: ignore
         return Vec2(
             x=self.x + other.x,
             y=self.y + other.y)
 
-    def __sub__(self, other):
+    def __sub__(self, other: 'Vec2') -> 'Vec2':
         return Vec2(
             x=self.x - other.x,
             y=self.y - other.y)
 
-    def __mul__(self, scalar):
+    def __mul__(self, scalar: float) -> 'Vec2':
         if type(scalar) != float and type(scalar) != int:
             raise TypeError("Can only multiply vector with scalars")
         return Vec2(
             x=self.x*scalar,
             y=self.y*scalar)
 
-    def __rmul__(self, scalar):
+    def __rmul__(self, scalar:float) -> 'Vec2':
         return self * scalar
 
-    def __div__(self, scalar):
+    def __truediv__(self, scalar:float) -> 'Vec2':
         if type(scalar) != float and type(scalar) != int:
             raise TypeError("Can only divide vector with scalars")
         return Vec2(
             x=self.x/scalar,
             y=self.y/scalar)
 
-    def __rdiv__(self, scalar):
+    def __rdiv__(self, scalar:float) -> 'Vec2':
         return self / scalar
 
-    def dot(self, other):
+    def dot(self, other:'Vec2') -> float:
         return self.x * other.x + self.y * other.y
 
-    def rotate(self, angle):
+    def rotate(self, angle:float) -> 'Vec2':
         s = math.sin(angle)
         c = math.cos(angle)
         return Vec2(
             x=self.x * c - self.y * s,
             y=self.x * s + self.y * c)
 
-    def normalized(self):
+    def normalized(self) -> 'Vec2':
         return self / self.length
 
-    def angleBetween(self, other):
+    def angleBetween(self, other:'Vec2') -> float:
         return math.acos(self.normalized().dot(other.normalized()))
 
-    def projectionOn(self, other):
+    def projectionOn(self, other:'Vec2') -> float:
         return self.dot(other.normalized())
     
-    def distance(self, other):
+    def distance(self, other:'Vec2') -> float:
         return (self - other).length
 
 class Transform(namedtuple('Transform', ['heading', 'offset'])):
@@ -92,35 +92,35 @@ class Transform(namedtuple('Transform', ['heading', 'offset'])):
     _identity = None
 
     @classmethod
-    def identity(cls):
+    def identity(cls) -> 'Transform':
         if not cls._identity:
             cls._identity=Transform(0, Vec2.zero())
         return cls._identity
 
     @staticmethod
-    def rotation(angle):
+    def rotation(angle) -> 'Transform':
         return Transform(heading = angle, offset = Vec2.zero())
     
     @staticmethod
-    def translation(vector):
+    def translation(vector) -> 'Transform':
         return Transform(heading = 0, offset = vector)
 
     @property
-    def x(self):
+    def x(self) -> float:
         return self.offset.x
 
     @property
-    def y(self):
+    def y(self) -> float:
         return self.offset.y
 
-    def applyTo(self, vector):
+    def applyTo(self, vector: Vec2) -> Vec2:
         return vector.rotate(self.heading) + self.offset
 
-    def after(self, other):
+    def after(self, other:'Transform') -> 'Transform':
         return Transform( \
             heading = self.heading + other.heading, \
             offset = self.applyTo(other.offset))
 
-    def inverse(self):
+    def inverse(self) -> 'Transform':
         return Transform.rotation(-self.heading)\
             .after(Transform.translation(-self.offset))
