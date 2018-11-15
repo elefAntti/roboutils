7cc9e536ad5f08a68bd0e45cb327ef0ed8dc038c
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-11-15 17:59:03 +0200

    moves angleDiff function to a new place

--- roboutils/utils/math_utils.py
+++ roboutils/utils/math_utils.py
@@ -14,3 +14,12 @@ def deg2rad(angle:float) -> float:
 
 def rad2deg(angle:float) -> float:
     return angle * 180 / math.pi
+
+# uses radians
+# if starting from angle1, 
+# how to turn in order to reach angle2
+def angleDiff(angle1:float, angle2:float) -> float:
+    angle1 = angle1 % (2*math.pi)
+    angle2 = angle2 % (2*math.pi)
+    diff = angle1-angle2
+    return diff if diff<math.pi else diff-2*math.pi
